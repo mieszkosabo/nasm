171b3b2ae0f9e78f879665214a6302a1e7bc43d1
Mieszko Sabo <ms406322@students.mimuw.edu.pl>
2020-04-08 20:51:16 +0200

    getSjForN połowicznie

--- PIX/pix.asm
+++ PIX/pix.asm
@@ -19,20 +19,21 @@ section .text
 %endmacro
 
 %macro modPower 0
-  mov    r8,rdx ; p do r8 wkładamy
-  mov    rax,rdi ; x do rax'a, bo będziemy brać resztę z dzielenia
+  ;mov    r8,rdx ; p do r8 wkładamy
+  ;mov    rax,rdi ; x do rax'a, bo będziemy brać resztę z dzielenia
+  mov    r12, rsi ; y <- n-k
   xor    edx,edx ; zerujemy rdx
   div    r8      ; dzielimy przez p
   test   rdx,rdx ; w rdx mamy resztę z dzielenia, sprawdzamy czy niezerową
   mov    r9,rdx  ; przenosimy ją do r9
   je     %%exit  ; jeśli to zero to out
-  test   rsi,rsi ; sprawdzamy czy y jest > 0
+  test   r12,r12 ; sprawdzamy czy y jest > 0
   je     %%one      ; jeśli mamy podnieść zioma do 0, outujemy
   mov    rcx,rdx ; przenosimy x do rcx
   mov    r9d,0x1 ; wkładamy 1 fo r9
   nop
 %%odd:
-  test   sil,0x1 ; sprawdzanie parzystości y-ka
+  test   r12b,0x1 ; sprawdzanie parzystości y-ka
   je     %%even 
   mov    rax,r9  ; jeśli y nieparzysty, to res = res * x
   xor    edx,edx
@@ -44,70 +45,42 @@ section .text
   xor    edx,edx
   mov    rax,rcx
   div    r8
-  shr    rsi,1    ; y = y / 2
+  shr    r12,1    ; y = y / 2
   mov    rcx,rdx
   jne    %%odd
 %%exit:
   mov    rax,r9
   jmp    %%end   
 %%one:
-  mov    r9d,0x1 
+  mov    rax,0x1 
 %%end:
 %endmacro
 
-; rdi ^ rsi % rdx, x ^ y % p
-power:
-  mov    r8,rdx ; p do r8 wkładamy
-  mov    rax,rdi ; x do rax'a, bo będziemy brać resztę z dzielenia
-  xor    edx,edx ; zerujemy rdx
-  div    r8      ; dzielimy przez p
-  test   rdx,rdx ; w rdx mamy resztę z dzielenia, sprawdzamy czy niezerową
-  mov    r9,rdx  ; przenosimy ją do r9
-  je     e1      ; jeśli to zero to out
-  test   rsi,rsi ; sprawdzamy czy y jest > 0
-  je     e8      ; jeśli mamy podnieść zioma do 0, outujemy
-  mov    rcx,rdx ; przenosimy x do rcx
-  mov    r9d,0x1 ; wkładamy 1 fo r9
-  nop
-b8:
-  test   sil,0x1 ; sprawdzanie parzystości y-ka
-  je     cd 
-  mov    rax,r9  ; jeśli y nieparzysty, to res = res * x
-  xor    edx,edx
-  imul   rax,rcx  ; mod p
-  div    r8
-  mov    r9,rdx
-cd:
-  imul   rcx,rcx  ; y (już) parzyste, więc x = x^2 % p
-  xor    edx,edx
-  mov    rax,rcx
-  div    r8
-  shr    rsi,1    ; y = y / 2
-  mov    rcx,rdx
-  jne    b8
-e1:
-  mov    rax,r9
-  ret    
-  nop    
-e8:
-  mov    r9d,0x1
-  jmp    e1 
-
-
 cntCj:
-
-
+  mov r15, rdi ; j do r15
+  xor ebp, ebp
+  mov r14, rsi ; n do r14
+  mov r8, rdi
+  xor r13, r13 ; miejsce na wynik
+loop:
+  mov rax, 16 ; stała do podniesienia do potęgi
+  modPower      ; w rax jest numerator
+  getDivFraction rax, r8 ; wynik w rax
+  add r13, rax  ; res += fraction
+  add rbp, 1    ; k++
+  mov r8, rbp   ; r8 = k
+  shl r8, 3     ; r8 = 8*k
+  add r8, r15   ; r8 = 8*k+j
+  sub rsi, 1    ; n-k
+  cmp rbp, r14  ; cmp k with n
+  jbe loop
+  mov rax, r13
+  ret
 
 pix:
   push rdi
-  ; mov r13, 200546
-  ; mov r8, 15
-  ; mov rdx, 33
-  mov rdi, 16
-  mov rsi, 100
-  mov rdx, 34
-  ;modPower r13, r8, rdx
-  modPower
-  ;call power
+  mov rdi, 6
+  mov rsi, 5
+  call cntCj
   pop rdi
   ret
