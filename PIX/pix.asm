a52a4e90d1991de7d4f89df1f805a7bfe4e7d7ab
Mieszko Sabo <ms406322@students.mimuw.edu.pl>
2020-05-17 23:44:36 +0200

    PIX

--- PIX/pix.asm
+++ PIX/pix.asm
@@ -3,52 +3,53 @@ global pix
 
 section .text
 
-; w rax mamy część "po przecinku"
+; first argument gets divided by second; fractional part ends up in rax
 %macro divFra 2
-  mov     rdx, %1                     ; argument do podzielenia
+  mov     rdx, %1                     
   xor     eax, eax
-  div     %2                          ; %2 to dzielnik, 64 bit register
+  div     %2                          
 %endmacro
-
+; multiplies two fractions; result in rax
 %macro mulFra 2
   mov     rax, %1
   mul     %2
 %endmacro
 
+; computes x^y % p, where x, y, p are in rax, rsi and r8; result in rax
 %macro modPower 0
-  mov     r12, rsi ; y <- n-k
-  test    r12,r12 ; sprawdzamy czy y jest > 0
-  je      %%one   ; jeśli mamy podnieść zioma do 0, outujemy z res 1
-  xor     edx,edx ; zerujemy rdx
-  div     r8      ; dzielimy przez p
-  test    rdx,rdx ; w rdx mamy resztę z dzielenia, sprawdzamy czy niezerową
-  mov     r9,rdx  ; przenosimy ją do r9
-  je      %%exit  ; jeśli to zero to out
+  mov     r12, rsi                    ; y <- n-k
+  test    r12,r12                     ; check if y > 0
+  je      %%one                       
+  xor     edx,edx 
+  div     r8                          ; divide by p
+  test    rdx,rdx                     ; check if remeinder is > 0
+  mov     r9,rdx  
+  je      %%exit                      ; if (x % p == 0) return 0
   
-  mov     rdi,rdx ; przenosimy x do (rdi)
-  mov     r9d,0x1 ; wkładamy 1 fo r9
+  mov     rdi,rdx                     ; move x to rdi
+  mov     r9d,0x1                     
   nop
 %%odd:
-  test    r12b,0x1 ; sprawdzanie parzystości y-ka
+  test    r12b,0x1                    ; check if y is even
   je      %%even 
-  mov     rax,r9  ; jeśli y nieparzysty, to res = res * x
+  mov     rax,r9                      ; if y is odd, then res = res * x
   xor     edx,edx
-  imul    rax,rdi  ; mod p
+  imul    rax,rdi                     ; mod p
   div     r8
   mov     r9,rdx
 %%even:
-  imul    rdi,rdi  ; y (już) parzyste, więc x = x^2 % p
+  imul    rdi,rdi                     ; if y is even, then x = x^2 % p
   xor     edx,edx
   mov     rax,rdi
   div     r8
-  shr     r12,1    ; y = y / 2
+  shr     r12,1                       ; y = y / 2
   mov     rdi,rdx
   jne     %%odd
 %%exit:
   mov     rax,r9
   jmp     %%end   
 %%one:
-  mov     rax, 0x1
+  mov     rax, 0x1                    ; x^0 % p = if p == 1 then 0 else 1
   xor     edi, edi
   cmp     r8, 1 
   cmove   eax, edi     
@@ -56,66 +57,66 @@ section .text
 %endmacro
 
 ; first arg is j, second n
-cntCj:
-  mov     r15, rdi ; j do r15
-  xor     ebp, ebp ; k = 0, licznik
+cntSj:
+  mov     r11, rdi                    ; put j in r11
+  xor     ebp, ebp                    ; k = 0, counter
   mov     r8, rdi
-  xor     r13, r13 ; miejsce na wynik
+  xor     r10, r10                    ; result in r13
 loop:
-  mov     rax, 16 ; stała do podniesienia do potęgi
-  modPower      ; w rax jest numerator
-  divFra  rax, r8 ; wynik w rax
-  add     r13, rax  ; res += fraction
-  add     rbp, 1    ; k++
-  mov     r8, rbp   ; r8 = k
-  shl     r8, 3     ; r8 = 8*k
-  add     r8, r15   ; r8 = 8*k+j
-  sub     rsi, 1    ; n-k
-  cmp     rbp, rcx  ; cmp k with n
+  mov     rax, 16                     ; const
+  modPower      
+  divFra  rax, r8                     ; result in rax
+  add     r10, rax                    ; res += fraction
+  add     rbp, 1                      ; k++
+  mov     r8, rbp                     ; r8 = k
+  shl     r8, 3                       ; r8 = 8*k
+  add     r8, r11                     ; r8 = 8*k+j
+  sub     rsi, 1                      ; n-k
+  cmp     rbp, rcx                    ; cmp k with n
   jbe     loop
 
-  mov     rsi, 0x1000000000000000   ; {1/16}
-  mov     r12, rsi ; currPow i num
+  mov     rsi, 0x1000000000000000     ; {1/16}
+  mov     r12, rsi                    ; r12 - 16 raised to folowing powers
 loop2:
-  mov     rax, r12  ; do num wkładam curPow
+  mov     rax, r12  
   xor     rdx, rdx
-  div     r8        ; curPow / 8k+j
+  div     r8                          ; current power of 16 / 8k+j
   test    rax, rax
   jz      end
-  add     r13, rax  ; res += curPart
+  add     r10, rax                    ; res += curPart
   xor     rdx, rdx
   mulFra  r12, rsi
-  mov     r12, rdx ; przenoszę wynik do r12
-  add     rbp, 1    ; k++
-  mov     r8, rbp   ; r8 = k
-  shl     r8, 3     ; r8 = 8*k
-  add     r8, r15   ; r8 = 8*k+j
+  mov     r12, rdx                    
+  add     rbp, 1                      ; k++
+  mov     r8, rbp                     ; r8 = k
+  shl     r8, 3                       ; r8 = 8*k
+  add     r8, r11                     ; r8 = 8*k+j
   jmp     loop2
 end:
-  mov     rax, r13
+  mov     rax, r10
   ret
 
-; n is parameter
+; n is a parameter here
 %macro BBP 1
   mov     rdi, 1
   mov     rsi, %1
-  call    cntCj
+  call    cntSj
   lea     rbx, [rax+rax*1]
 
   mov     rdi, 4
   mov     rsi, %1
-  call    cntCj
+  call    cntSj
   sub     rbx, rax
   add     rbx, rbx
 
   mov     rdi, 5
   mov     rsi, %1
-  call    cntCj
+  call    cntSj
   sub     rbx, rax
 
   mov     rdi, 6
   mov     rsi, %1
-  call    cntCj
+  call    cntSj
   sub     rbx, rax
 %endmacro
 
@@ -137,30 +138,40 @@ end:
   pop     rbx
 %endmacro
 
-align 8
-pix:
-  pushRegs
-  mov     r11, rdx    ; max na r11
-  mov     r14, rsi     ; r14 to &m
-  mov     r10, rdi    ; tablica na r10
+%macro ptime 0
+  rdtsc
+  mov     rdi, rdx
+  shl     rdi, 32
+  add     rdi, rax
+  call pixtime
+%endmacro
 
+pix:
+  sub rsp, 0x8
+  pushRegs
+  mov     r15, rdx                    ; max in r15
+  mov     r14, rsi                    ; &pid in r14
+  mov     r13, rdi                    ; array ptr in r13
+  ptime
   mov     rcx, 1
   lock \
   xadd    [r14], rcx
-  cmp     rcx, r11
+  cmp     rcx, r15
   jae     finish
 repeat:
-  lea     rcx, [rcx*8+0x0] ; rcx = 8*m
-  BBP     rcx        ; w rbx pojawia się 64 bit wynik
-  shr     rbx, 32   ; rbx >> 32, starsze 32 bity na ebx
+  lea     rcx, [rcx*8+0x0]            ; rcx = 8*m
+  BBP     rcx                         ; 64 bit result in rbx
+  shr     rbx, 32                     ; rbx >> 32
   shr     rcx, 1
-  mov     dword[r10 + rcx], ebx
+  mov     dword[r13 + rcx], ebx
   mov     rcx, 1
   lock \
   xadd    [r14], rcx
-  cmp     rcx, r11
+  cmp     rcx, r15
   jb      repeat
 finish:
+  ptime
   xor     eax, eax
   popRegs
+  add rsp, 0x8
   ret
